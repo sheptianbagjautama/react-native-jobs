9316bcd34167b86f32c96389623aaeb1ad9c6e15
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-23 05:37:02 +0700

    layout index app

--- app/index.js
+++ app/index.js
@@ -1,11 +1,47 @@
-import { View, Text } from 'react-native';
+import { useState } from "react";
+import { View, Text } from "react-native";
+import { Stack, useRouter } from "expo-router";
 
+import { COLORS, icons, images, SIZES } from "../constants";
+import {
+  Nearbyjobs,
+  Popularjobs,
+  ScreenHeaderBtn,
+  Welcome,
+} from "../components";
+import { SafeAreaView } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 const Home = () => {
-    return (
-        <View>
-            <Text>Home</Text>
+  const router = useRouter();
+
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+      <Stack.Screen
+        options={{
+          headerStyle: {
+            backgroundColor: COLORS.lightWhite,
+          },
+          headerTitle: "",
+          headerTitleAlign: "center",
+          headerShadowVisible: false,
+          headerLeft: () => (
+            <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+          ),
+          headerRight: () => (
+            <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+          ),
+        }}
+      />
+
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={{ flex: 1, padding: SIZES.medium }}>
+          <Welcome />
+          <Popularjobs />
+          <Nearbyjobs />
         </View>
-    )
-}
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 export default Home;
