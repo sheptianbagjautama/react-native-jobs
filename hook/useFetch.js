208acc158b9a2cd7ee6184ef2337443a92d84e0c
Sheptian Bagja Utama <sheptianbagjautama@gmail.com>
2023-03-23 23:41:29 +0700

    Develop UI an Consume PopularJob Component and Card

---
+++ hook/useFetch.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+// import { RAPID_API_KEY } from "@env";
+
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    params: { ...query },
+    headers: {
+      "X-RapidAPI-Key": "0a42dc32f8mshf03f16e6d66f617p11fb29jsna26dea794a54",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+};
+
+export default useFetch;
